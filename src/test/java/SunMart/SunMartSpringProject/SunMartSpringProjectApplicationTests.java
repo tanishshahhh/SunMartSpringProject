b372ec8b9c8a53aadcc499f08f9ea3e8f7a4fe4b
TANISH <shahtanish18@gamil.com>
2026-03-27 13:22:17 +0530

    Initial commit

---
+++ src/test/java/SunMart/SunMartSpringProject/SunMartSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package SunMart.SunMartSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunMartSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
